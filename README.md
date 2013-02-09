6016ad00669c587fb2d55ab117134b2f6f49debf
Todd Turner <todd@turnernet.org>
2013-02-09 15:00:11 -0700

    update readme, clean up formatting

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ tts-proxy
 =========
 
 Example server that proxies requests to google TTS.
+
+Usage:  http://<server>:8000/q=hello+world&tl=en
--- tts-proxy.js
+++ tts-proxy.js
@@ -9,10 +9,9 @@ http.createServer(function (req, res) {
 	var parameters = url.parse(req.url,true);
 	console.log(parameters.query)
 
-	var options = { method: 'GET'
-    , uri: "http://translate.google.com/translate_tts?q="+parameters.query['q']+'&tl='+parameters.query['tl'],
+	var options = { method: 'GET', uri: "http://translate.google.com/translate_tts?q="+parameters.query['q']+'&tl='+parameters.query['tl'],
 	headers : { "user-agent":"Mozilla"}	
-    }
+	}
 	
 	request(options, function(error, response, buffer) {
 		//console.log(error)
